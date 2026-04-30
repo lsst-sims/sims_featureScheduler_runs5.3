d2bab0ac53f224d450af47d665b6cf286bb5f202
Peter Yoachim <yoachim@uw.edu>
2026-04-29 20:30:42 -0700

    roll mash

---
+++ roll_mash/fbs_config.py
@@ -0,0 +1,411 @@
+__all__ = ("get_scheduler",)
+
+import copy
+import hashlib
+import os
+import pathlib
+
+import numpy as np
+import rubin_scheduler.scheduler.basis_functions as bf
+import rubin_scheduler.scheduler.detailers as detailers
+from rubin_scheduler.data import get_data_dir
+from rubin_scheduler.scheduler.schedulers import BaseQueueManager, CoreScheduler
+from rubin_scheduler.scheduler.surveys import ScriptedSurvey
+from rubin_scheduler.scheduler.utils import (
+    CurrentAreaMap,
+    Footprint,
+    ScheduledObservationArray,
+)
+from rubin_scheduler.site_models import Almanac
+from fudge_rolling import make_rolling_footprints
+
+# import lsst.ts.fbs.utils.maintel.lsst_surveys as lsst_surveys
+# import lsst.ts.fbs.utils.maintel.roman_surveys as roman_surveys
+# import lsst.ts.fbs.utils.maintel.too_surveys as too_surveys
+import lsst_surveys as lsst_surveys
+import roman_surveys as roman_surveys
+import too_surveys as too_surveys
+from survey_start import SURVEY_START_MJD
+
+
+def generate_qm() -> BaseQueueManager:
+    """Generate a QueueManager object."""
+
+    detailer_list = []
+    detailer_list.append(detailers.RotspUpdateDetailer())
+    bf_list = []
+    # This should get zenith masked
+    bf_list.append(bf.SlewtimeBasisFunction())
+    # Any clouds that have rolled in
+    bf_list.append(bf.MapCloudBasisFunction())
+    qm = BaseQueueManager(detailers=detailer_list, basis_functions=bf_list)
+
+    return qm
+
+
+def get_scheduler() -> tuple[int, CoreScheduler]:
+    """Construct the LSST survey scheduler.
+
+    The parameters are not accessible when calling as 'config'.
+
+    Returns
+    -------
+    nside : `int`
+        Healpix map resolution.
+    scheduler : `rubin_scheduler.scheduler.scheduler.CoreScheduler`
+        Feature based scheduler.
+    """
+    nside = 32
+    science_program = "BLOCK-419"
+    band_to_filter = {
+        "u": "u_24",
+        "g": "g_6",
+        "r": "r_57",
+        "i": "i_39",
+        "z": "z_20",
+        "y": "y_10",
+    }
+    exptime = 30
+    nexp = 1
+    u_exptime = 38
+    u_nexp = 1
+
+    template_exptime = 30
+    u_template_exptime = 38
+
+    survey_start_mjd = SURVEY_START_MJD
+
+    # Safety mask parameters - constraints on all survey pointings
+    # Generally shadow_minutes value is set by the survey, but can
+    # be set here as well (will be overwritten if too short for survey).
+    safety_mask_params = {
+        "nside": nside,
+        "wind_speed_maximum": 40,
+        "apply_time_limited_shadow": False,
+        "time_to_sunrise": 3.0,
+        "min_az_sunrise": 150,
+        "max_az_sunrise": 250,
+    }
+
+    safety_mask_params_ddf = copy.deepcopy(safety_mask_params)
+    safety_mask_params_ddf["shadow_minutes"] = 30
+
+    # General parameters for standard pairs (-80/80 default)
+    camera_rot_limits = (-60.0, 60.0)
+    pair_time = 33
+    # Adjust these as the expected timing updates.
+    # -- sets the expected time and number of pointings in a 'blob'.
+    blob_survey_params = {
+        "slew_approx": 8,
+        "band_change_approx": 140.0,
+        "read_approx": 3.07,
+        "flush_time": 30.0,
+        "smoothing_kernel": None,
+        "nside": nside,
+        "seed": 42,
+        "twilight_scale": True,
+    }
+    # Seeing (FWHM in ") max for template
+    fwhm_template_max = 1.2
+
+    # Parameters for rolling cadence footprint definition
+    nslice = 2  # N slices for rolling
+    rolling_scale = 0.9  # Strength of rolling
+    rolling_uniform = True  # Should we use the uniform rolling flag
+
+    # Parameters for long-gaps survey
+    nights_off = 3  # For long gaps
+
+    # Parameters for near-sun twilight microsurvey
+    ei_night_pattern = 4  # see pattern_dict below
+    ei_bands = "riz"  # Bands to use for earth interior observations.
+    ei_repeat = 4  # Number of times to repeat earth interior observations
+    ei_am = 2.5  # Earth interior airmass limit
+    ei_elong_req = 45.0  # Solar elongation required for inner solar system
+    ei_area_req = 0.0  # Sky area required before attempting inner solar system
+
+    # Mapping for night_pattern for near-sun twilight / twi_blob surveys.
+    pattern_dict = {
+        1: [True],
+        2: [True, False],
+        3: [True, False, False],
+        4: [True, False, False, False],
+        5: [True, True, True, True, False, False, False, False],
+        6: [True, True, True, False, False, False, False],
+        7: [True, True, False, False, False, False],
+    }
+    ei_night_pattern = pattern_dict[ei_night_pattern]
+
+    # Generate footprint over the sky
+    sky = CurrentAreaMap(nside=nside)
+    footprints_hp_array, labels = sky.return_maps()
+    # Identify pixels for rolling
+    roll_indx = np.where((labels == "lowdust") | (labels == "virgo"))[0]
+    roll_footprint = footprints_hp_array["r"] * 0
+    roll_footprint[roll_indx] = 1
+
+    footprints_hp = {}
+    for key in footprints_hp_array.dtype.names:
+        footprints_hp[key] = footprints_hp_array[key]
+
+    # Set up a mask to contain some surveys within this region
+    footprint_mask = footprints_hp["r"] * 0
+    footprint_mask[np.where(footprints_hp["r"] > 0)] = 1
+
+    # Use the Almanac to find the position of the sun at the start of survey
+    almanac = Almanac(mjd_start=survey_start_mjd)
+    sun_moon_info = almanac.get_sun_moon_positions(survey_start_mjd)
+    sun_ra_start = sun_moon_info["sun_RA"].copy()
+
+    # Define the rolling footprint
+    footprints = make_rolling_footprints(
+        fp_hp=footprints_hp,
+        mjd_start=survey_start_mjd,
+        sun_ra_start=sun_ra_start,
+        nslice=nslice,
+        scale=rolling_scale,
+        nside=nside,
+        wfd_indx=roll_indx,
+        order_roll=1,
+        n_cycles=3,
+        uniform=rolling_uniform,
+    )
+
+    # Define the long-gaps (triplets) survey.
+    gaps_night_pattern = [True] + [False] * nights_off
+    long_gaps = lsst_surveys.gen_long_gaps_survey(
+        footprints=footprints,
+        nside=nside,
+        camera_rot_limits=camera_rot_limits,
+        exptime=exptime,
+        nexp=nexp,
+        u_exptime=u_exptime,
+        u_nexp=u_nexp,
+        pair_time=pair_time,
+        night_pattern=gaps_night_pattern,
+        science_program=science_program,
+        blob_survey_params=blob_survey_params,
+        safety_mask_params=safety_mask_params,
+    )
+
+    # This hash is provided by the script that
+    # generates the pre-computed data. Execute it and paste
+    # the provided value here.
+    expected_hex_digest = "58950b2"
+    pre_comp_file = (
+        pathlib.Path(get_data_dir())
+        / "scheduler"
+        / f"ts_ddf_array_{expected_hex_digest}.npz"
+    )
+    if os.path.exists(pre_comp_file):
+        loaded = np.load(pre_comp_file, allow_pickle=True)
+        hash_object = hashlib.sha256()
+        hash_object.update(loaded["hash_digest"])
+        hex_digest = hash_object.hexdigest()[:7]
+        if hex_digest == expected_hex_digest:
+            obs_array_loaded = loaded["obs_array"]
+            obs_array = ScheduledObservationArray(obs_array_loaded.size)
+            for key in obs_array_loaded.dtype.names:
+                obs_array[key] = obs_array_loaded[key]
+        else:
+            raise RuntimeError(
+                f"Provided hash {expected_hex_digest} does not match loaded file hash {hex_digest}. "
+                "Reach out for support so they can help you generate the correct file."
+            )
+        loaded.close()
+    else:
+        raise RuntimeError(
+            f"Pre-computed DDF files {pre_comp_file} not available. "
+            "Reach out for support so they can help execute the script "
+            "that will generate this file before proceeding."
+        )
+
+    # Parameters for  DDF dithers
+    camera_ddf_rot_limit = 55  # Rotator limit for DDF (degrees) .. 75
+    camera_ddf_rot_per_visit = 3.0  # small rotation per visit (degrees) .. 3
+    max_dither = 0.2  # Max radial dither for DDF (degrees)
+    per_night = False  # Dither DDF per night (True) or per visit (False)
+
+    band_expt = {
+        "u": u_exptime,
+        "g": exptime,
+        "r": exptime,
+        "i": exptime,
+        "z": exptime,
+        "y": exptime,
+    }
+    band_nexp = {"u": u_nexp, "g": nexp, "r": nexp, "i": nexp, "z": nexp, "y": nexp}
+    u_nexp = band_nexp["u"]
+    u_exptime = band_expt["u"]
+
+    detailer_list = [
+        detailers.CameraSmallRotPerObservationListDetailer(
+            min_rot=-camera_ddf_rot_limit,
+            max_rot=camera_ddf_rot_limit,
+            per_visit_rot=camera_ddf_rot_per_visit,
+        ),
+        detailers.SplitDetailer(
+            detailers.DitherDetailer(per_night=per_night, max_dither=max_dither),
+            detailers.EuclidDitherDetailer(per_night=per_night),
+        ),
+        detailers.BandNexp(bandname="u", nexp=u_nexp, exptime=u_exptime),
+        detailers.BandSortDetailer(),
+        detailers.LabelRegionsAndDDFs(),
+        detailers.TruncatePreTwiDetailer(),
+    ]
+
+    ddfs = [
+        ScriptedSurvey(
+            lsst_surveys.safety_masks(**safety_mask_params_ddf),
+            nside=nside,
+            detailers=detailer_list,
+            survey_name="deep drilling",
+            before_twi_check=False,
+        )
+    ]
+    ddfs[0].set_script(obs_array)
+
+    # Define the greedy surveys (single-visit per call)
+    greedy = lsst_surveys.gen_greedy_surveys(
+        nside=nside,
+        camera_rot_limits=camera_rot_limits,
+        exptime=exptime,
+        nexp=nexp,
+        u_exptime=u_exptime,
+        u_nexp=u_nexp,
+        footprints=footprints,
+        science_program=science_program,
+        safety_mask_params=safety_mask_params,
+    )
+
+    # Define the near-sun twilight microsurvey
+    neo_micro = lsst_surveys.generate_twilight_near_sun(
+        nside=nside,
+        night_pattern=ei_night_pattern,
+        max_airmass=ei_am,
+        camera_rot_limits=camera_rot_limits,
+        footprint_mask=footprint_mask,
+        min_area=ei_area_req,
+        bands=ei_bands,
+        n_repeat=ei_repeat,
+        max_elong=ei_elong_req,
+        science_program=science_program,
+        safety_mask_params=safety_mask_params,
+    )
+
+    # Define the standard pairs during the night survey
+    blobs = lsst_surveys.generate_blobs(
+        footprints=footprints,
+        nside=nside,
+        camera_rot_limits=camera_rot_limits,
+        exptime=exptime,
+        nexp=nexp,
+        u_exptime=u_exptime,
+        u_nexp=u_nexp,
+        pair_time=pair_time,
+        survey_start=survey_start_mjd,
+        science_program=science_program,
+        blob_survey_params=blob_survey_params,
+        safety_mask_params=safety_mask_params,
+    )
+
+    # Define Roman scripted surveys
+    roman_micro = [
+        roman_surveys.gen_roman_on_season(
+            nside=nside,
+            max_dither=max_dither,
+            per_night=per_night,
+            camera_ddf_rot_limit=camera_ddf_rot_limit,
+            camera_ddf_rot_per_visit=camera_ddf_rot_per_visit,
+            exptimes=exptime,
+            nexps=nexp,
+            science_program=science_program,
+            safety_mask_params=safety_mask_params,
+        ),
+        roman_surveys.gen_roman_off_season(
+            nside=nside,
+            max_dither=max_dither,
+            per_night=per_night,
+            camera_ddf_rot_limit=camera_ddf_rot_limit,
+            camera_ddf_rot_per_visit=camera_ddf_rot_per_visit,
+            exptimes=exptime,
+            nexps=nexp,
+            science_program=science_program,
+            safety_mask_params=safety_mask_params,
+        ),
+    ]
+
+    # Create template footprint.
+    # Similar to rolling footprint but tracks visits separately
+    # (only good seeing visits) and no rolling.
+    template_fp = Footprint(survey_start_mjd, sun_ra_start, nside=nside)
+    for key in footprints_hp_array.dtype.names:
+        tmp_fp = np.where(footprints_hp_array[key] > 0, 1, np.nan)
+        template_fp.set_footprint(key, tmp_fp)
+    # Define template surveys
+    template_surveys = lsst_surveys.gen_template_surveys(
+        template_fp,
+        nside=nside,
+        band1s=["u", "g", "g", "r", "r", "i", "r", "z", "y"],
+        band2s=["u", "g", "r", "r", "i", "z", "z", "y", "y"],
+        seeing_fwhm_max=fwhm_template_max,
+        camera_rot_limits=camera_rot_limits,
+        exptime=template_exptime,
+        nexp=nexp,
+        u_exptime=u_template_exptime,
+        u_nexp=u_nexp,
+        n_obs_template={"u": 6, "g": 6, "r": 6, "i": 6, "z": 6, "y": 6},
+        science_program=science_program,
+        blob_survey_params=blob_survey_params,
+        safety_mask_params=safety_mask_params,
+    )
+
+    # Define ToO surveys
+    too_detailers = []
+    too_detailers.append(
+        detailers.CameraRotDetailer(
+            min_rot=np.min(camera_rot_limits), max_rot=np.max(camera_rot_limits)
+        )
+    )
+    too_detailers.append(detailers.BandSortDetailer())
+    too_detailers.append(detailers.LabelRegionsAndDDFs())
+    # Let's make a footprint to follow up ToO events
+    too_footprint = np.where(footprints_hp["r"] > 0, 1.0, np.nan)
+
+    toos = too_surveys.gen_too_surveys(
+        nside=nside,
+        detailer_list=too_detailers,
+        too_footprint=too_footprint,
+        n_snaps=nexp,
+        science_program=science_program,
+        safety_mask_params=safety_mask_params,
+    )
+
+    # Arrange the surveys in tiers.
+    surveys = [
+        toos,
+        roman_micro,
+        ddfs,
+        template_surveys,
+        long_gaps,
+        blobs,
+        neo_micro,
+        greedy,
+    ]
+
+    qm = generate_qm()
+
+    # Combine into CoreScheduler
+    scheduler = CoreScheduler(
+        surveys,
+        nside=nside,
+        survey_start_mjd=survey_start_mjd,
+        band_to_filter=band_to_filter,
+        queue_manager=qm,
+    )
+
+    return nside, scheduler
+
+
+if __name__ == "config":
+    nside, scheduler = get_scheduler()
---
+++ roll_mash/fudge_rolling.py
@@ -0,0 +1,203 @@
+
+import numpy as np
+from rubin_scheduler.scheduler.utils import Footprint, Footprints, CurrentAreaMap, StepSlopes, slice_quad_galactic_cut
+import copy
+
+from rubin_scheduler.utils import DEFAULT_NSIDE
+
+
+def make_rolling_footprints(
+    fp_hp=None,
+    mjd_start=60218.0,
+    sun_ra_start=3.27717639,
+    nslice=2,
+    scale=0.8,
+    nside=DEFAULT_NSIDE,
+    wfd_indx=None,
+    order_roll=0,
+    n_cycles=None,
+    n_constant_start=2,
+    n_constant_end=6,
+    verbose=False,
+    uniform=True,
+    kludge=True,
+):
+    """
+    Generate rolling footprints
+
+    Parameters
+    ----------
+    fp_hp : dict-like
+        A dict with band name keys and HEALpix map values.
+        Default None will load CurrentAreaMap. Assumes
+        WFD is where r-band is 1.
+    mjd_start : `float`
+        The starting date of the survey.
+    sun_ra_start : `float`
+        The RA of the sun at the start of the survey
+    nslice : `int`
+        How much to slice the sky up. Can be 2, 3, 4, or 6.
+    scale : `float`
+        The strength of the rolling, value of 1 is full power rolling.
+        Zero is no rolling.
+    wfd_indx : array of ints
+        The indices of the HEALpix map that are to be included in the rolling.
+    order_roll : `int`
+        Change the order of when bands roll. Default 0.
+    n_cycles : `int`
+        Number of complete rolling cycles to attempt. If None, defaults to 3
+        full cycles for nslice=2, 2 cycles for nslice=3 or 4, and 1 cycle for
+        nslice=6.
+    n_constant_start : `int`
+        The number of constant non-rolling seasons to start with. Anything
+        less than 2 will start rolling too early near Y1. Defaults to 2.
+    n_constant_end : `int`
+        The number of constant seasons to end the survey with. Defaults to 6.
+    kludge : `bool`
+        Horrible kludge to mix and match behavior so constant season 
+        added to the start and first constant season after rolling
+        is removed.
+
+    Returns
+    -------
+    Footprints object
+    """
+
+    if fp_hp is None:
+        sky = CurrentAreaMap(nside=nside)
+        footprints, labels = sky.return_maps()
+        fp_hp = {}
+        for key in footprints.dtype.names:
+            fp_hp[key] = footprints[key]
+
+    nc_default = {2: 3, 3: 2, 4: 2, 6: 1}
+    if n_cycles is None:
+        n_cycles = nc_default[nslice]
+
+    hp_footprints = fp_hp
+
+    D = 1.0 - scale
+    U = nslice - D * (nslice - 1)
+
+    start = [1.0] * n_constant_start
+    # After n_cycles, just go to no-rolling for 6 years.
+    end = [1.0] * n_constant_end
+
+    rolling = [U] + [D] * (nslice - 1)
+    rolling = np.roll(rolling, order_roll).tolist()
+
+    all_slopes = []
+    if uniform:
+        extra_constant = [1]
+    else:
+        extra_constant = []
+
+    for i in range(nslice):
+        _roll = np.roll(rolling, i).tolist() + extra_constant
+        all_slopes.append(start + _roll * n_cycles + end)
+    for i in range(nslice):
+        _roll = np.roll(rolling, i).tolist() + extra_constant
+        _roll = [_roll[-1]] + _roll[1:-1] + [_roll[0]]
+        all_slopes.append(start + _roll * n_cycles + end)
+    
+    if verbose:
+        dvals = {
+        1: "1",
+        D: "D",
+        U: "U",
+    }
+        abc = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        slice_names = ["slice %s" % abc[i] for i in range(nslice)]
+        for i, s in enumerate(all_slopes):
+            if i >= nslice:
+                sname = slice_names[i - nslice] + " w/ ra - sun_ra in [90, 270]"
+            else:
+                sname = slice_names[i] + " w/ ra - sun_ra in [270, 90]"
+            
+            print(sname + ": " + " ".join([dvals[x] for x in s]))
+
+    fp_non_wfd = Footprint(mjd_start, sun_ra_start=sun_ra_start, nside=nside)
+    rolling_footprints = []
+    
+
+    # Insert a constant season at the start, remove the first constant season after rolling
+    if kludge:
+        new_all_slopes = []
+        for slopes in all_slopes:
+            new_slopes = copy.copy(slopes)
+            new_slopes = [1.0] + new_slopes
+            # which constant to remove?
+            indx = np.where((np.diff(new_slopes) != 0) & (np.array(new_slopes)[1:] == 1))[0].min()
+            del new_slopes[indx + 1]
+            new_all_slopes.append(new_slopes)
+        all_slopes = new_all_slopes
+
+    for i in range(len(all_slopes)):
+        step_func = StepSlopes(rise=all_slopes[i])
+        rolling_footprints.append(
+            Footprint(
+                mjd_start,
+                sun_ra_start=sun_ra_start,
+                step_func=step_func,
+                nside=nside,
+            )
+        )
+
+    wfd = hp_footprints["r"] * 0
+    if wfd_indx is None:
+        wfd_indx = np.where(hp_footprints["r"] == 1)[0]
+
+    wfd[wfd_indx] = 1
+    non_wfd_indx = np.where(wfd == 0)[0]
+
+    if uniform:
+        split_wfd_indices = slice_quad_galactic_cut(
+            hp_footprints,
+            nslice=nslice,
+            wfd_indx=wfd_indx,
+            ra_range=(sun_ra_start + 1.5 * np.pi, sun_ra_start + np.pi / 2),
+        )
+
+        split_wfd_indices_delayed = slice_quad_galactic_cut(
+            hp_footprints,
+            nslice=nslice,
+            wfd_indx=wfd_indx,
+            ra_range=(sun_ra_start + np.pi / 2, sun_ra_start + 1.5 * np.pi),
+        )
+    else:
+        split_wfd_indices = slice_quad_galactic_cut(hp_footprints, nslice=nslice, wfd_indx=wfd_indx)
+
+    for key in hp_footprints:
+        temp = hp_footprints[key] + 0
+        temp[wfd_indx] = 0
+        fp_non_wfd.set_footprint(key, temp)
+
+        for i in range(nslice):
+            # make a copy of the current band
+            temp = hp_footprints[key] + 0
+            # Set the non-rolling area to zero
+            temp[non_wfd_indx] = 0
+
+            indx = split_wfd_indices[i]
+            # invert the indices
+            ze = temp * 0
+            ze[indx] = 1
+            temp = temp * ze
+            rolling_footprints[i].set_footprint(key, temp)
+        if uniform:
+            for _i in range(nslice, nslice * 2):
+                # make a copy of the current band
+                temp = hp_footprints[key] + 0
+                # Set the non-rolling area to zero
+                temp[non_wfd_indx] = 0
+
+                indx = split_wfd_indices_delayed[_i - nslice]
+                # invert the indices
+                ze = temp * 0
+                ze[indx] = 1
+                temp = temp * ze
+                rolling_footprints[_i].set_footprint(key, temp)
+
+    result = Footprints([fp_non_wfd] + rolling_footprints)
+    return result
+
