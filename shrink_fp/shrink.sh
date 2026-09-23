7ffc54bc96f64127d365caae831de4163acd61b2
Peter Yoachim <yoachim@uw.edu>
2026-09-23 11:53:23 -0700

    0.1 footprint

--- shrink_fp/shrink.sh
+++ shrink_fp/shrink.sh
@@ -1,6 +1,7 @@
-python shrink_fp.py --dust_limit 0.25
-python shrink_fp.py --dust_limit 0.199
-python shrink_fp.py --dust_limit 0.15
-python shrink_fp.py --dust_limit 0.12
-python shrink_fp.py --dust_limit 0.08
-python shrink_fp.py --dust_limit 0.05
+# python shrink_fp.py --dust_limit 0.25
+# python shrink_fp.py --dust_limit 0.199
+# python shrink_fp.py --dust_limit 0.15
+# python shrink_fp.py --dust_limit 0.12
+# python shrink_fp.py --dust_limit 0.08
+# python shrink_fp.py --dust_limit 0.05
+python shrink_fp.py --dust_limit 0.10
